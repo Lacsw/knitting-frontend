43dfa14b93f023b705d689824b0f0a56c782b837
frolovrv <frolloff1@yandex.ru>
2022-01-13 22:09:43 +0500

    add Category pages, create ProductBox component

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -53,7 +53,7 @@ export default {
             const product_slug = this.$route.params.product_slug
 
             await axios
-                .get(`/api/v1/product/${category_slug}/${product_slug}`)
+                .get(`/api/v1/products/${category_slug}/${product_slug}`)
                 .then(response => {
                     this.product = response.data
 
