7418d23b64da55d02fa7dfd3a10d8ce27fa4db2c
frolovrv <frolloff1@yandex.ru>
2022-01-14 17:37:19 +0500

    create base Cart page

---
+++ src/views/Cart.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="page-cart">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">Your Cart</h1>
+            </div>
+            <div class="column is-12 box">
+                <table class="table is-fullwidth" v-if="cartTotalLength">
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+
+                <p v-else>You don't have any products in your cart...</p>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Cart'
+}
+</script>
