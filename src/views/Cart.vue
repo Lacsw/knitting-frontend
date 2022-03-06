d09fc6d5b44c0b970d43f89050a4af5c51a16eec
frolovrv <frolloff1@yandex.ru>
2022-03-06 15:59:23 +0300

    Cart view and CartItem as a component

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -17,11 +17,24 @@
                     </thead>
 
                     <tbody>
-                        
+                        <CartItem
+                            v-for="item in cart.items"
+                            v-bind:key="item.product.id"
+                            v-bind:initialItem="item" />
                     </tbody>
                 </table>
 
                 <p v-else>You don't have any products in your cart...</p>
+            </div>
+
+            <div class="column is-12 box">
+                <h2 class="subtitle">Summary</h2>
+
+                <strong>${{ cartTotalPrice.toFixed(2) }}</strong>, {{ cartTotalLength }} items
+                
+                <hr>
+
+                <router-link to="/cart/checkout" class="button is-dark">Proceed to checkout</router-link>
             </div>
         </div>
     </div>    
@@ -30,7 +43,34 @@
 <script>
 import axios from 'axios'
 
+import CartItem from '@/components/CartItem.vue'
+
 export default {
-    name: 'Cart'
+    name: 'Cart',
+    components: {
+        CartItem
+    },
+    data() {
+        return {
+            cart: {
+                items: []
+            }
+        }
+    },
+    mounted() {
+        this.cart = this.$store.state.cart
+    },
+    computed: {
+        cartTotalLength() {
+            return this.cart.items.reduce((acc, curVal) => {
+                return acc += curVal.quantity
+            }, 0)
+        },
+        cartTotalPrice() {
+            return this.cart.items.reduce((acc, curVal) => {
+                return acc += curVal.product.price * curVal.quantity
+            }, 0)
+        },
+    }
 }
 </script>
